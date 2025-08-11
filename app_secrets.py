76dfa656ae051c3ac43a01217973e6723261e5ca
Dave Cowden <dave.cowden@gmail.com>
2025-08-10 23:04:35 -0400

    well that as wrong

--- app_secrets.py
+++ app_secrets.py
@@ -1,17 +1,11 @@
 
 from dynaconf import Dynaconf, Validator
-DEFAULT_CONFIG = {
-    "LOCAL_DB": True,
-    "LOGINS": {
-        "admin": "a",
-        "student": "s"
-    }
-}
+
 SETTINGS = Dynaconf(
-    settings_files=['.streamlit/secrets.toml'], environments=True,envvar_prefix="APP",
+    settings_files=['.streamlit/config.toml','.streamlit/secrets.toml'], environments=True,envvar_prefix="APP",
     validators=[
         Validator("LOCAL_DB",is_type_of=bool)
     ])
-SETTINGS.update(DEFAULT_CONFIG)
+
 SETTINGS.validators.validate()
 print(f"Starting Settings: {SETTINGS.as_dict()}")
